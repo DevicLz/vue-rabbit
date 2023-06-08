a758fababab38526e97fa96115399fca0a76f09c
DevicLz <1113573320@qq.com>
2023-06-08 11:27:48 +0800

    本地购物车-头部购物车

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -1,5 +1,6 @@
 <script setup >
 import { usecategoryStore } from '@/stores/category';
+import HeaderCart from './HeaderCart.vue'
 const categoryStore = usecategoryStore()
 </script>
 
@@ -22,7 +23,7 @@ const categoryStore = usecategoryStore()
         <input type="text" placeholder="搜一搜">
       </div>
       <!-- 头部购物车 -->
-      
+      <HeaderCart />
     </div>
   </header>
 </template>
